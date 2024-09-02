2676f3f1ba998aa79ac8f3d92b67baeb3410c959
lulugore <mellohanna.2006@gmail.com>
2024-09-02 00:53:02 -0300

    Creating main loop Connecting Menu and Game Classes

---
+++ code/Level.py
@@ -0,0 +1,11 @@
+class Level:
+
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+        self.entity_list = None
+
+
+    def run(self, ):
+        pass
---
+++ code/Player.py
@@ -0,0 +1,16 @@
+from Entity import Entity
+
+
+
+class Player(Entity):
+
+    def __init__(self):
+        pass
+
+
+    def update(self, ):
+        pass
+
+
+    def move(self, ):
+        pass
